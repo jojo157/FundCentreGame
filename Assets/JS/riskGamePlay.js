11d22bca3a8f04817d5146e6181438a845ddbd8e
jojo157 <carrigj@tcd.ie>
2021-04-03 12:57:57 +0000

    functions created to display random fund on screen for fund risk game

--- Assets/JS/riskGamePlay.js
+++ Assets/JS/riskGamePlay.js
@@ -10,7 +10,8 @@ setTimeout(function(){
     console.log(fundName[3]);}, 500);
 setTimeout(function(){
     console.log(fundRisk[3]);}, 500);
-
+setTimeout(function(){
+    displayFundQuestion();}, 500);
 
 function extractDataFromTxtFile(fileAddress, arrayVariable){
      $.get(fileAddress, function(data) {
@@ -19,4 +20,23 @@ function extractDataFromTxtFile(fileAddress, arrayVariable){
             arrayVariable.push(elem);
         });
      });
+     return arrayVariable;
+}
+
+function generateRandomIndex(){
+    let randomIndex = Math.floor(Math.random() *77 ) +1;
+    if (randomIndex === 1){
+        randomIndex = randomIndex + 1;
+    }
+    return randomIndex;
+}
+
+function generateRandomFund(){
+    let index = generateRandomIndex();
+    let fundChoosen = fundName[index];
+    return fundChoosen;
+}
+
+function displayFundQuestion(){
+    document.getElementById("fund-name-text").textContent = generateRandomFund();
 }
