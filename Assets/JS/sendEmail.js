359b94915d6609245985ffc8d190413cd0603fe8
jojo157 <carrigj@tcd.ie>
2021-04-08 19:32:59 +0000

    added contact page with basic form, added emailJS API to send form details when user clicks submit

---
+++ Assets/JS/sendEmail.js
@@ -0,0 +1,19 @@
+$('#contact-form').on('submit', function(event) {
+    event.preventDefault(); // prevent reload
+    
+    var formData = new FormData(this);
+    formData.append('service_id', 'service_d7aqa1n');
+    formData.append('template_id', 'template_3e1u6tq');
+    formData.append('user_id', 'user_SWI5POi95flikT5s70ZfL');
+ 
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+        type: 'POST',
+        data: formData,
+        contentType: false, // auto-detection
+        processData: false // no need to parse formData to string
+    }).done(function() {
+        alert('Your mail is sent!');
+    }).fail(function(error) {
+        alert('Oops... ' + JSON.stringify(error));
+    });
+});
