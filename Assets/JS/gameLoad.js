787a54501ac57182feb9509df14cb906d495e38b
jojo157 <carrigj@tcd.ie>
2021-04-15 20:27:45 +0000

    added alert when on IE browser to use other browser and tidied up code

--- Assets/JS/gameLoad.js
+++ Assets/JS/gameLoad.js
@@ -1,83 +1,111 @@
 /*jshint esversion: 6 */
-const $ = window.$ ;
-
-$(document).ready(function(){
-
-    var gameChosen = "" ;
-    var levelChosen = "";
-    let myPromise = new Promise(function(myResolve, myReject) {
-        
-        if(gameChosen === ""){
-          myResolve("complete loading"); 
-        }else{
-          myReject("failed"); 
+const $ = window.$;
+$(document).ready(function () {
+    let isIE11 = /Trident.*rv[ :]*11\./.test(navigator.userAgent);
+    if (isIE11) {
+        window.alert("This game is incompatible with IE11 please use another browser");
+    }
+    // Get IE or Edge browser version
+    let version = detectIE();
+    console.log("version: ", version);
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
+    function detectIE() {
+        let ua = window.navigator.userAgent;
+        // IE 11
+        // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+        let msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
         }
-        });
-        
-        myPromise.then(
-          function(value) { 
+        let trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            let rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+        // other browser
+        return false;
+    }
+    let gameChosen = "";
+    let levelChosen = "";
+    let myPromise = new Promise(function (myResolve, myReject) {
+        if (gameChosen === "") {
+            myResolve("complete loading");
+        } else {
+            myReject("failed");
+        }
+    });
+    myPromise.then(
+        function () {
             setUpGame(gameChosen, levelChosen);
-          },
-          function(error) { console.log("error loading game"); }
-        );
-        });
-    
-
-
-
-
-function setUpGame(gameChosen, levelChosen){
-let games = document.getElementsByClassName("game-StartGame");
-    for( let game of games){
-        game.addEventListener("click",function(){
+        },
+        function (error) {
+            console.log("error loading game: ", error);
+        }
+    );
+});
+function setUpGame(gameChosen, levelChosen) {
+    let games = document.getElementsByClassName("game-StartGame");
+    for (let game of games) {
+        game.addEventListener("click", function () {
             gameChosen = this.id;
             displayModal();
-            return(gameChosen);
+            return (gameChosen);
         });
     }
-    
-
     let chosenLevel = document.getElementsByClassName("button-StartGame");
-    for(let level of chosenLevel){
-        level.addEventListener("click", function(){
-            let myLevelButton = this.textContent;
-            levelChosen = myLevelButton;
-
-            if(gameChosen === "fund-risk"){
-            loadRiskGame(levelChosen);
-            } else if (gameChosen === "fund-management-game"){
-            loadManagementGame(levelChosen); 
-        }
-        });}
-  
+    for (let level of chosenLevel) {
+        level.addEventListener("click", function () {
+            levelChosen = this.textContent;
+            if (gameChosen === "fund-risk") {
+                loadRiskGame(levelChosen);
+            } else if (gameChosen === "fund-management-game") {
+                loadManagementGame(levelChosen);
+            }
+        });
+    }
 }
-    
-    function loadRiskGame(myLevelButton){
-        if(myLevelButton === "Easy"){
-            window.open("gameRisk.html?variable=Easy", "_self");
-        }
-        else if(myLevelButton === "Medium"){
-            window.open("gameRisk.html?variable=Medium", "_self");
-        }
-        else{
-            window.open("gameRisk.html?variable=Hard", "_self");
-        }
+function loadRiskGame(myLevelButton) {
+    if (myLevelButton === "Easy") {
+        window.open("gameRisk.html?variable=Easy", "_self");
+    } else if (myLevelButton === "Medium") {
+        window.open("gameRisk.html?variable=Medium", "_self");
+    } else {
+        window.open("gameRisk.html?variable=Hard", "_self");
     }
+}
+function loadManagementGame(myLevelButton) {
+    if (myLevelButton === "Easy") {
+        window.open("gameManagement.html?variable=Easy", "_self");
+    } else if (myLevelButton === "Medium") {
+        window.open("gameManagement.html?variable=Medium", "_self");
+    } else {
+        window.open("gameManagement.html?variable=Hard", "_self");
+    }
+}
+function displayModal() {
+    document.getElementById("setup-Game-Title").innerText = "Lets Get Started";
+    document.getElementById("setup-text-message").innerText = "Game selected, now choose your level. Are you up for a challenge!";
+    document.getElementById("setup-game-modal").click();
+}
 
-    function loadManagementGame(myLevelButton){
-        if(myLevelButton === "Easy"){
-            window.open("gameManagement.html?variable=Easy", "_self");
-        }
-        else if(myLevelButton === "Medium"){
-            window.open("gameManagement.html?variable=Medium", "_self");
-        }
-        else{
-            window.open("gameManagement.html?variable=Hard", "_self");
-        }
-    }
 
-    function displayModal(){
-        document.getElementById("setup-Game-Title").innerText = "Lets Get Started";
-        document.getElementById("setup-text-message").innerText = "Game selected, now choose your level. Are you up for a challenge!";
-        document.getElementById("setup-game-modal").click();
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assets/JS/managementGamePlay.js
+++ Assets/JS/managementGamePlay.js
@@ -1,239 +1,193 @@
 /*jshint esversion: 6 */
-const $ = window.$ ;
-
-var fundName =[];
-var fundManagement = [];
-var fundsInGame = [];
-var soundToPlay = document.createElement("audio");
-
- 
-$(document).ready(function(){
+const $ = window.$;
+const fundName = [];
+const fundManagement = [];
+const fundsInGame = [];
+const soundToPlay = document.createElement("audio");
+$(document).ready(function () {
     extractDataFromTxtFile('Assets/FundTextFiles/fundsForManagement.txt', fundName);
-    extractDataFromTxtFile('Assets/FundTextFiles/fundManagementStyle.txt', fundManagement);   
+    extractDataFromTxtFile('Assets/FundTextFiles/fundManagementStyle.txt', fundManagement);
 });
-
 $(window).on("load", function startup() {
-    setTimeout(function(){
-    startGame();
-    let buttons = document.getElementsByClassName("answer-button");
-    let reset = document.getElementById("management-restart-button");
-
-    reset.addEventListener("click", function(){
+    setTimeout(function () {
+        startGame();
+        let buttons = document.getElementsByClassName("answer-button");
+        let reset = document.getElementById("management-restart-button");
+        reset.addEventListener("click", function () {
             restartGame();
-        });    
-
-    for(let button of buttons){
-            button.addEventListener("click", function(){
-            checkAnswer(this.textContent);     
         });
-    }},500);
-    
-
+        for (let button of buttons) {
+            button.addEventListener("click", function () {
+                checkAnswer(this.textContent);
+            });
+        }
+    }, 500);
 });
-
-function startGame(){
+function startGame() {
     gameLevel();
     displayFundQuestion();
     numberOfQuestionsPerGame();
-    
 }
-
-
-function extractDataFromTxtFile(fileAddress, arrayVariable){
-     $.get(fileAddress, function(data) {
+function extractDataFromTxtFile(fileAddress, arrayVariable) {
+    $.get(fileAddress, function (data) {
         var lines = data.split("\n");
-        $.each(lines, function(n, elem) {
+        $.each(lines, function (n, elem) {
             arrayVariable.push(elem);
         });
-     });
-     return arrayVariable;
+    });
+    return arrayVariable;
 }
-
-function generateRandomIndex(){
-    let randomIndex = Math.floor(Math.random() *74 ) +1;
-    return randomIndex;
+function generateRandomIndex() {
+    return Math.floor(Math.random() * 74) + 1;
 }
-
-function generateRandomFund(){
+function generateRandomFund() {
     let index = generateRandomIndex();
     let fundChoosen = fundName[index];
     let correctManagement = fundManagement[index];
     let solutions = [];
-    solutions[0]=fundChoosen;
-    solutions[1]= correctManagement;
-
+    solutions[0] = fundChoosen;
+    solutions[1] = correctManagement;
     let repeatQ = checkRepeatQuestion(fundChoosen);
-    
-    if(repeatQ === true){
+    if (repeatQ === true) {
         generateRandomFund();
     }
-    if(repeatQ === false){
-    fundsInGame.push(fundChoosen);
+    if (repeatQ === false) {
+        fundsInGame.push(fundChoosen);
     }
     return solutions;
-    
 }
-
-function displayFundQuestion(){
+function displayFundQuestion() {
     let callSolutions = generateRandomFund();
     document.getElementById("management-fund-name-text").textContent = callSolutions[0];
-    let managementAnswer = callSolutions[1] ;
-    return managementAnswer;
-    
+    return callSolutions[1];
 }
-
-function checkAnswer(useranswer){
+function checkAnswer(useranswer) {
     let endValue = document.getElementById("management-totalNumberOfQuestions").textContent;
     let currentValue = document.getElementById("management-questionsAnswered").textContent;
-
-    if(endValue === currentValue){
-        return ;
-    }else{
-
-    let fund = document.getElementById("management-fund-name-text").textContent;
-    let indexFund = fundName.indexOf(fund);
-    let managementFund = fundManagement[indexFund];
-    managementFund = managementFund.trim();
-    let isCorrect = useranswer === managementFund;
-    if(isCorrect){
-        playCorrectSound();
-        
-        setTimeout(function(){
-            answerAlert(isCorrect,managementFund);
-            updateCorrectScore();
-            updateNumberOfQsAnswered();
-            checkGameEnd();},1000); }
-    else{
-        playWrongSound();
-
-        setTimeout(function(){
-        answerAlert(isCorrect,managementFund);
-        updateIncorrectScore();
-        updateNumberOfQsAnswered();
-        checkGameEnd();
-        },1000);
-    }  
-    if(fundsInGame.length < parseInt(document.getElementById("management-totalNumberOfQuestions").innerText)){
-        setTimeout(function(){
-            nextQuestion();}, 2000);
+    if (endValue === currentValue) {
+        return;
+    } else {
+        let fund = document.getElementById("management-fund-name-text").textContent;
+        let indexFund = fundName.indexOf(fund);
+        let managementFund = fundManagement[indexFund];
+        managementFund = managementFund.trim();
+        let isCorrect = useranswer === managementFund;
+        if (isCorrect) {
+            playCorrectSound();
+            setTimeout(function () {
+                answerAlert(isCorrect, managementFund);
+                updateCorrectScore();
+                updateNumberOfQsAnswered();
+                checkGameEnd();
+            }, 1000);
+        } else {
+            playWrongSound();
+            setTimeout(function () {
+                answerAlert(isCorrect, managementFund);
+                updateIncorrectScore();
+                updateNumberOfQsAnswered();
+                checkGameEnd();
+            }, 1000);
+        }
+        if (fundsInGame.length < parseInt(document.getElementById("management-totalNumberOfQuestions").innerText)) {
+            setTimeout(function () {
+                nextQuestion();
+            }, 2000);
+        }
     }
 }
+function updateCorrectScore() {
+    let previousScore = parseInt(document.getElementById("management-correct-score").innerText);
+    previousScore = (previousScore + 1).toString();
+    document.getElementById("management-correct-score").innerText = previousScore;
 }
-
-function updateCorrectScore(){
- let previousScore = parseInt(document.getElementById("management-correct-score").innerText);
- previousScore = previousScore + 1;
- document.getElementById("management-correct-score").innerText = previousScore;
+function updateIncorrectScore() {
+    let previousScore = parseInt(document.getElementById("management-incorrect-score").innerText);
+    previousScore = (previousScore + 1).toString();
+    document.getElementById("management-incorrect-score").innerText = previousScore;
 }
-
-function updateIncorrectScore(){
- let previousScore = parseInt(document.getElementById("management-incorrect-score").innerText);
- previousScore = previousScore + 1;
- document.getElementById("management-incorrect-score").innerText = previousScore;
-}
-
-function nextQuestion(){
+function nextQuestion() {
     displayFundQuestion();
 }
-
-function restartGame(){
-        document.getElementById("management-correct-score").innerText = 0;
-        document.getElementById("management-incorrect-score").innerText = 0;
-        document.getElementById("management-questionsAnswered").innerText = 0;
-        while(fundsInGame.length > 0) {
-            fundsInGame.pop();
-        }
-        nextQuestion();
-      
+function restartGame() {
+    document.getElementById("management-correct-score").innerText = '0';
+    document.getElementById("management-incorrect-score").innerText = '0';
+    document.getElementById("management-questionsAnswered").innerText = '0';
+    while (fundsInGame.length > 0) {
+        fundsInGame.pop();
+    }
+    nextQuestion();
 }
-
-function gameLevel(){
-   let level =  window.location.href;
-   level = level.split("=");
-   level = level[1];
-   return level;
+function gameLevel() {
+    let level = window.location.href;
+    level = level.split("=");
+    level = level[1];
+    return level;
 }
-
-function numberOfQuestionsPerGame(){
+function numberOfQuestionsPerGame() {
     let level = gameLevel();
     let numberOfQs;
-    if(level ==="Easy"){
+    if (level === "Easy") {
         numberOfQs = 6;
         document.getElementById("management-totalNumberOfQuestions").innerText = numberOfQs;
-    }else if(level ==="Medium"){
+    } else if (level === "Medium") {
         numberOfQs = 12;
         document.getElementById("management-totalNumberOfQuestions").innerText = numberOfQs;
-    }else{
+    } else {
         numberOfQs = 18;
         document.getElementById("managementtotalNumberOfQuestions").innerText = numberOfQs;
     }
-
 }
-
-function updateNumberOfQsAnswered(){
+function updateNumberOfQsAnswered() {
     let numQuestion = document.getElementById("management-questionsAnswered").textContent;
     numQuestion = parseInt(numQuestion) + 1;
     document.getElementById("management-questionsAnswered").innerText = numQuestion;
 }
-
-function checkGameEnd(){
+function checkGameEnd() {
     let endValue = document.getElementById("management-totalNumberOfQuestions").textContent;
     let currentValue = document.getElementById("management-questionsAnswered").textContent;
-    if(currentValue === endValue){
-        setTimeout(function(){endGame();},10);
+    if (currentValue === endValue) {
+        setTimeout(function () {
+            endGame();
+        }, 10);
     }
 }
-
-function endGame(){
+function endGame() {
     let endValue = document.getElementById("management-totalNumberOfQuestions").textContent;
     let currentCorrect = document.getElementById("management-correct-score").textContent;
-    let score ;
+    let score;
     endValue = parseInt(endValue);
     currentCorrect = parseInt(currentCorrect);
-    score = (((currentCorrect / endValue) * 100).toFixed(0)) + "%"  ; 
-    
+    score = (((currentCorrect / endValue) * 100).toFixed(0)) + "%";
     document.getElementById("management-Game-Title").innerText = "Congratulations";
     document.getElementById("management-answer-text-message").innerText = `Game Over! Your score is ${score}. Check out the Fund Centre Website to learn more! Why not try and beat your score.`;
     document.getElementById("management-game-modal").click();
     document.getElementById("management-fund-name-text").innerText = `Game Over! Your score is ${score}`;
-   
 }
-
-function checkRepeatQuestion(choosenFund){
-    for(let i=0; i<fundsInGame.length; i++){
-        if(fundsInGame[i] === choosenFund){
+function checkRepeatQuestion(choosenFund) {
+    for (let i = 0; i < fundsInGame.length; i++) {
+        if (fundsInGame[i] === choosenFund) {
             return true;
         }
     }
-
-    return false ;
-    
-
+    return false;
 }
-
-
-function answerAlert(isCorrect,managementFund){
-    if(isCorrect){
-    document.getElementById("management-Game-Title").innerText = "Congratulations";
-    document.getElementById("management-answer-text-message").innerText = "Well Done, you got it right. Keep it up!";
-    document.getElementById("management-game-modal").click();
+function answerAlert(isCorrect, managementFund) {
+    if (isCorrect) {
+        document.getElementById("management-Game-Title").innerText = "Congratulations";
+        document.getElementById("management-answer-text-message").innerText = "Well Done, you got it right. Keep it up!";
+        document.getElementById("management-game-modal").click();
+    } else {
+        document.getElementById("management-Game-Title").innerText = "Hard Luck";
+        document.getElementById("management-answer-text-message").innerText = `The correct answer is ${managementFund}.`;
+        document.getElementById("management-game-modal").click();
     }
-    else{
-    document.getElementById("management-Game-Title").innerText = "Hard Luck";
-    document.getElementById("management-answer-text-message").innerText = `The correct answer is ${managementFund}.`;
-    document.getElementById("management-game-modal").click();
-    }
-    return;
 }
-    
-function playCorrectSound(){
+function playCorrectSound() {
     soundToPlay.setAttribute("src", "Assets/Sounds/correct.wav");
     soundToPlay.play();
-    return;
 }
-
-function playWrongSound(){
+function playWrongSound() {
     soundToPlay.setAttribute("src", "Assets/Sounds/wrong.wav");
     soundToPlay.play();
-    return;
 }
