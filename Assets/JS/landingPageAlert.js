8f9b7ca5bdf9dfc3ba23c06aa827095c0fc43275
jojo157 <carrigj@tcd.ie>
2021-04-16 07:50:11 +0000

    fixed navigator.appName conditonal statement

--- Assets/JS/landingPageAlert.js
+++ Assets/JS/landingPageAlert.js
@@ -1,7 +1,11 @@
 /*jshint esversion: 6 */
-const $ = window.$;
+
+
 $(document).ready(function () {
-    if(navigator.appName = "Microsoft Internet Explorer"){
+    if(navigator.appName == "Microsoft Internet Explorer"){
         alert("This game is incompatible with IE11 please use another browser");
     }
-});
+    else{
+        alert("not IE");
+    }
+})
