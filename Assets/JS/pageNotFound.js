90f1cf3cac379b13be46ec27018950a12bf93bb0
jojo157 <carrigj@tcd.ie>
2021-04-18 11:29:58 +0000

    404.html page styled and button on click event added to bring user to home page

---
+++ Assets/JS/pageNotFound.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+const $ = window.$ ;
+
+$(document).ready(function () {
+    document.getElementById("home-button-404").addEventListener("click", function () {
+        window.open("index.html", "_self");
+    });
+
+});
